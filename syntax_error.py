5a76095283fb250b2455723d4b6ea5ea75c6b5db
NoHunGry <tzuyang1991@gmail.com>
2020-11-01 01:09:14 +0800

    error flow step001

---
+++ syntax_error.py
@@ -0,0 +1,11 @@
+# 語法錯誤範例
+if __name__ == "__main__":
+    print(r"This is example for 'Syntax Error'")
+    print()
+    print "it's will trigger 'Syntax Error'"
+
+    varA = int(10)
+    varB = "blablablabla"
+    sample_list = []
+    sample_list.append(varA)
+    sample_list.append(varB)
